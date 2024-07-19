4cd76c97a5a05c12464a577454c1de37e9d01c91
y-ohgi <ohgi.211@gmail.com>
2024-07-19 14:21:19 +0900

    init

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  # region = "us-west-2"
+  region = "ap-northeast-1"
+}
+
+variable "az" {
+  type = list(string)
+  default = [ "ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d" ]
+}
+
+variable "type" {
+  type = string
+  default = "t3.micro"
+}
+
+resource "aws_instance" "web" {
+  for_each = var.az
+
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = var.type
+
+  tags = {
+    Name = "HelloWorld-${each}"
+  }
+}
