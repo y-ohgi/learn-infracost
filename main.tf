ef022289239760ca5eba8e4eee20585307a8b1ab
y-ohgi <ohgi.211@gmail.com>
2024-07-19 14:28:32 +0900

    fix: ec2 type

--- main.tf
+++ main.tf
@@ -19,7 +19,9 @@ resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = var.type
 
-  tags = {
+  tags_all = {
     Name = "HelloWorld-${each}"
+    Service = each
+    Environment = each
   }
 }
